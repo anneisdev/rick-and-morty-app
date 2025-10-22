58e6e0087224cbed423ad83a025b631d801b8813
Anne Lüken <anne.lkn@hotmail.com>
2025-10-22 14:53:45 +0200

    fixed character info aligment

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -19,12 +19,18 @@ export function createCharacterCard(character) {
           <div class="card__content">
             <h2 class="card__title">${name}</h2>
             <dl class="card__info">
-              <dt class="card__info-title">Status</dt>
-              <dd class="card__info-description">${status}</dd>
-              <dt class="card__info-title">Type</dt>
-              <dd class="card__info-description">${type}</dd>
-              <dt class="card__info-title">Occurrences</dt>
-              <dd class="card__info-description">${occurence}</dd>
+              <div class="card__seperators">
+                <dt class="card__info-title">Status</dt>
+                <dd class="card__info-description">${status}</dd>
+              </div>
+              <div class="card__seperators">
+                <dt class="card__info-title">Type</dt>
+                <dd class="card__info-description">${type}</dd>
+              </div>
+              <div class="card__seperators">
+                <dt class="card__info-title">Occurrences</dt>
+                <dd class="card__info-description">${occurence}</dd>
+              </div>
             </dl>
           </div>`;
 
