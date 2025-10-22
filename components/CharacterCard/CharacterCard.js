07667610da2efa2dc3696a9d799caa2cea842706
Elisa Linnemannstoens <elisalin2001@gmail.com>
2025-10-22 14:11:14 +0200

    finished searchbar

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,32 @@
-export function createCharacterCard() {}
+export function createCharacterCard(character) {
+  const newCard = document.createElement("li");
+
+  const name = character.name;
+  const status = character.status;
+  const type = character.type;
+  const occurence = character.episode.length;
+  const img = character.image;
+
+  newCard.innerHTML = `
+          <div class="card__image-container">
+            <img
+              class="card__image"
+              src="${img}"
+              alt="Rick Sanchez"
+            />
+            <div class="card__image-gradient"></div>
+          </div>
+          <div class="card__content">
+            <h2 class="card__title">${name}</h2>
+            <dl class="card__info">
+              <dt class="card__info-title">Status</dt>
+              <dd class="card__info-description">${status}</dd>
+              <dt class="card__info-title">Type</dt>
+              <dd class="card__info-description">${type}</dd>
+              <dt class="card__info-title">Occurrences</dt>
+              <dd class="card__info-description">${occurence}</dd>
+            </dl>
+          </div>`;
+
+  return newCard;
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import { createCharacterCard } from "./components/CharacterCard/CharacterCard.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
+export const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
 const searchBar = document.querySelector('[data-js="search-bar"]');
@@ -9,6 +11,46 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+let maxPage = 1;
+let page = 1;
+let searchQuery = "";
+
+export async function fetchCharacters() {
+  const apiUrl = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${page}&name=${searchQuery}`
+  );
+  const data = await apiUrl.json();
+
+  const characters = data.results;
+
+  cardContainer.innerHTML = "";
+
+  characters.forEach((character) => {
+    const card = createCharacterCard(character);
+    cardContainer.append(card);
+  });
+
+  console.log(characters);
+}
+
+fetchCharacters();
+
+function Search() {
+  searchBar.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    if (data.query) {
+      searchQuery = data.query;
+    } else {
+      searchQuery = "";
+    }
+
+    page = 1;
+    console.log(data);
+    fetchCharacters();
+  });
+}
+Search();
