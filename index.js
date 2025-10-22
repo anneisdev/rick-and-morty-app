7ef1ceae0f93c1f794fb70a8bae080740dfde466
Anne Lüken <anne.lkn@hotmail.com>
2025-10-22 14:18:17 +0200

    somehow had to fix merge conflcit even though it was already fixed

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 import { createCharacterCard } from "./components/CharacterCard/CharacterCard.js";
-<<<<<<< HEAD
-import { updatePagination } from "./components/NavPagination/NavPagination.js";
-=======
->>>>>>> searchbar
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 export const searchBarContainer = document.querySelector(
@@ -15,24 +11,6 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-<<<<<<< HEAD
-export let page = 1;
-export let maxPage;
-const searchQuery = "";
-
-export function setPage(newPage) {
- page = newPage; 
-}
-
-export async function fetchCharacters() {
-  const apiUrl = await fetch(
-    `https://rickandmortyapi.com/api/character/?page=${page}`
-  );
-  const data = await apiUrl.json();
-
-  maxPage = data.info.pages;
-  let characters = data.results;
-=======
 let maxPage = 1;
 let page = 1;
 let searchQuery = "";
@@ -44,7 +22,6 @@ export async function fetchCharacters() {
   const data = await apiUrl.json();
 
   const characters = data.results;
->>>>>>> searchbar
 
   cardContainer.innerHTML = "";
 
@@ -53,13 +30,6 @@ export async function fetchCharacters() {
     cardContainer.append(card);
   });
 
-<<<<<<< HEAD
-  console.log(data);
-}
-
-await fetchCharacters();
-updatePagination();
-=======
   console.log(characters);
 }
 
@@ -84,4 +54,3 @@ function Search() {
   });
 }
 Search();
->>>>>>> searchbar
